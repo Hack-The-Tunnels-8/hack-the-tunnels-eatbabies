1348f491bd94ed6cf8fcd43c6205ca6d22d353d5
Zaine Moodie <zainemoodie@gmail.com>
2023-09-30 16:56:26 -0400

    Issue: #11, added logo, fixed navbar, changed buttons, and added padding

--- client/src/components/Navbar/Navbar.tsx
+++ client/src/components/Navbar/Navbar.tsx
@@ -2,6 +2,8 @@ import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 import { useAccountContext } from "../../context";
 import "./Navbar.style.scss";
+import logo from "./EATbabies.png";
+
 
 function Navbar() {
   const { loggedIn, logout } = useAccountContext();
@@ -10,16 +12,30 @@ function Navbar() {
   return (
     <div className="navbar">
       <div className="navbar__logo">
-        <Link to="/">Store Logo</Link>
+        <Link to="/"><img src={logo} className="logo" alt="Logo"  /></Link>
       </div>
       <div className="navbar__account">
         {loggedIn() === false ? (
           <>
-            <button onClick={() => navigate("/sign-up")}>Sign Up</button>
-            <button onClick={() => navigate("/login")}>Login</button>
+            <button onClick={() => navigate("/sign-up")} 
+              style={{
+                backgroundColor: "#F67280",
+                borderRadius: 1,
+                padding:2,
+              }} className = "button">Sign Up</button>
+            <button onClick={() => navigate("/login")} 
+              style={{
+                backgroundColor: "#F67280",
+                borderRadius: 1,
+                padding:2,
+              }}>Login</button>
           </>
         ) : (
-          <button onClick={() => logout()}>Logout</button>
+          <button onClick={() => logout()}
+          style={{
+            backgroundColor: "#F67280",
+            borderRadius: 1,
+          }}>Logout</button>
         )}
       </div>
     </div>
